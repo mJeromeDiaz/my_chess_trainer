d18c6737e28ee8483853c5fd6475e63c5e1f38c5
jeromeDiaz <m.jerome.diaz@gmail.com>
2023-08-12 23:57:00 +0200

    store woodpecker

---
+++ front/src/stores/woodpecker.js
@@ -0,0 +1,11 @@
+import { defineStore } from 'pinia';
+
+export const woodpeckerStore = defineStore('woodpecker', {
+  state: () => {
+    return {
+      puzzles: []
+    }
+  },
+  actions: {
+  }
+)
